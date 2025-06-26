e36db87fb721047dc43454f175fa2bdf72856bb3
SSM TARIQ <syedtariqfiles@gmail.com>
2025-06-26 15:28:06 -0400

    Update worker_setup_remote.sh

--- src/processing/worker_setup_remote.sh
+++ src/processing/worker_setup_remote.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-parallel-scp -h $1 requirements.txt .
-parallel-scp -h $1 *.sh .
+parallel-scp -h $1 requirements.txt ~/
+parallel-scp -h $1 *.sh ~/
 
 parallel-ssh -t 0 -h $1 "bash ./install_conda.sh"
 parallel-ssh -t 0 -h $1 "export PATH=\$PATH:~/miniconda3/bin ; source ~/miniconda3/etc/profile.d/conda.sh ; conda init ; conda create -n p311 python=3.11 -y ; conda activate p311"
